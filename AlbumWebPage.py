5b76c6a7fabd27ec2bbb5141f62f454d1add9b07
jdmau72 <justindmau@gmail.com>
2025-06-21 17:14:52 -0600

    Successfully managed to pass the dominant colors into the html style via my flask python file

--- AlbumWebPage.py
+++ AlbumWebPage.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template("ColorThemeTest.html")
+    #get the colors from the image
+    colorA, colorB = findDominantColors("static/img/joni1.jpg", 7)
+    colorTheme = [f"rgb({colorA[0]},{colorA[1]},{colorA[2]})", f"rgb({colorB[0]},{colorB[1]},{colorB[2]})"]
+    return render_template("ColorThemeTest.html", colorTheme=colorTheme)
 
 if __name__ == '__main__':
     app.run(debug=True)
